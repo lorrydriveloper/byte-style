79d63a84e410cfefdeabc08224419675185baf71
lorrydriveloper <lorrydriveloper@gmail.com>
2021-10-24 15:26:26 +0100

    new attempt

--- lib/byte/style.rb
+++ lib/byte/style.rb
@@ -1,10 +1,9 @@
 require 'byte/style/version'
-
+require 'rails/generators/base'
 module Byte
   module Style
-    # Nothing to see here.
     class StylesGenerator < Rails::Generators::Base
-      desc 'This generator creates an initializer file at config/initializers'
+      desc 'This generator creates a style files for your project'
       def create_initializer_file
         create_file('.rubocop.yml', '# Add initialization content here')
         create_file('.prettierrc.yml', '# testing this.')
