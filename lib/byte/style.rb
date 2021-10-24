fbad330b2ff92ab8f6b3997035a8b8470b9031a0
lorrydriveloper <lorrydriveloper@gmail.com>
2021-10-24 15:59:57 +0100

    try copy files

--- lib/byte/style.rb
+++ lib/byte/style.rb
@@ -2,11 +2,13 @@ require 'byte/style/version'
 require 'rails/generators'
 module Byte
   module Style
-    class DevGenerator < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
       desc 'This generator creates a style files for your project'
-      def create_initializer_file
-        create_file('.rubocop.yml', '# Add initialization content here')
-        create_file('.prettierrc.yml', '# testing this.')
+      source_root File.expand_path('styles_config', __dir__)
+
+      def copy_initializer_file
+        copy_file('.prettierrc.yml', '.prettierrc.yml')
+        copy_file('.rubocop.yml', '.rubocop.yml')
       end
     end
   end
