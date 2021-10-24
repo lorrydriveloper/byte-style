7fcb32fbcceb913c4008d80ebd5b61dd7b4eb81c
lorrydriveloper <lorrydriveloper@gmail.com>
2021-10-24 15:49:43 +0100

    other approach

--- lib/byte/style.rb
+++ lib/byte/style.rb
@@ -2,7 +2,7 @@ require 'byte/style/version'
 require 'rails/generators/base'
 module Byte
   module Style
-    class StylesGenerator < Rails::Generators::Base
+    class DevGenerator < Rails::Generators::Base
       desc 'This generator creates a style files for your project'
       def create_initializer_file
         create_file('.rubocop.yml', '# Add initialization content here')
